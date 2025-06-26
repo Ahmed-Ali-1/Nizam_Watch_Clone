e71ab466048e6ef4a705badf8f1413044c4e3945
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-06-26 17:47:25 +0500

    last commit final

--- assets/js/owl.js
+++ assets/js/owl.js
@@ -1,11 +1,7 @@
-
-$('.owl-carousel').owlCarousel({
-    items:2.3,
-    loop:false,
-    margin:10,
-    nav:true,
-    dots:false,
-     
+$(".owl-carousel").owlCarousel({
+  items: 2.3,
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
 });
-
-
